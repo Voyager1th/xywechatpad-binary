e6e991609232f49071aab5b8691af5d0a9347144
HenryXiaoYang <henryyang666@hotmail.com>
2025-02-20 16:55:27 +0800

    feat: version update

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ if sys.version_info < (3, 11):
 
 setup(
     name="xywechatpad-binary",
-    version="1.0.1",
+    version="1.1.0",
     author="HenryXiaoYang",
     author_email="henryyang666@hotmail.com",
     description="XYBotV2 Binary Distribution",
